2382e99ab21a7442372d8aa9b6faacdd793d8f5f
Jessica Barros <61724191+jebarros@users.noreply.github.com>
2021-10-24 21:51:08 -0300

    Add files via upload

---
+++ HelpTask/src/main/java/com/example/HelpTask/HelpTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelpTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpTaskApplication.class, args);
+	}
+
+}
---
+++ HelpTask/src/test/java/com/example/HelpTask/HelpTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelpTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
